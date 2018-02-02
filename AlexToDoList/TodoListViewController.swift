02f87385d9f7051d926b8712ec0fb2fc34b7831f
Alex Pashkevych <alexpashkevych@alexs-mbp.fios-router.home>
2018-02-01 21:16:04 -0500

    All items saved after app closed

--- AlexToDoList/TodoListViewController.swift
+++ AlexToDoList/TodoListViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
 
     //MARK - TableView DataSource Methods
@@ -57,6 +64,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item" , style: .default) { (action) in
             //what will happen once the user clicked the Add button on UIAlert
             self.itemArray.append(textField.text ?? "New Item")
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
             
         }
